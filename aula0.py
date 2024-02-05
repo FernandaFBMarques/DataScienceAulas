b8b7d9f515acb42e9face3a23314692c7ed1b757
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-02-05 20:20:12 -0300

    commit inicial

---
+++ aula0.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt #quando eu colo o "as" ai uso oq coloquei depois dele para referenciar a biblio
+import seaborn as sns
+
+#a biblioteca seaborn tbm é uma opção
+
+notas = pd.read_csv("/Users/mariafernandafreitasbarbosamarques/Desktop/introducao-a-data-science-aula0/aula0/ml-latest-small/ratings.csv")
+
+#notas.head()
+#print(notas)
+#print(notas.shape)
+notas.columns = ["usuarioId", "filmeId", "nota", "momento"]
+#print(notas.head) #pandas dataframe
+
+#pandas series:
+#print(notas['nota'].unique()) #informa quais foram as notas
+#print(notas['nota'].value_counts()) #informa quantas vezes cada nota foi dada e ordenou do mais frequente para o menos
+#print(notas['nota'].mean()) #informa a média
+
+n = notas.nota
+n_mean = n.mean()
+n_median = n.median()
+#print(notas.nota)
+#n.matplotlib.pyplot.plot(kind='hist')
+#matplotlib.pyplot.show()
+#plt.hist(n) #plota oq eu quero, cria tabela
+#plt.show() #mostra a tabela
+
+print(n.describe()) #descreve as medidas
+sns.boxplot(n) #cria o boxplot
+plt.show() #display do boxplot
