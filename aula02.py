e54eb816c9eb1e0daa7e9c72d09002bd46018ea4
FernandaFBMarques <mfernandafbmarques@gmail.com>
2024-02-07 23:21:58 -0300

    Add from second class.0

--- aula02.py
+++ aula02.py
@@ -1,4 +1,10 @@
 import pandas as pd
 
-notas = pd.read_csv("/Users/mariafernandafreitasbarbosamarques/Desktop/introducao-a-data-science-aula0/aula0/ml-latest-small/ratings.csv")
+movies = pd.read_csv("movies.csv")
+ratings = pd.read_csv("ratings.csv")
 
+print(movies.head())
+print(ratings.query("movieId==1").rating)# mostra as notas somente do filme 1, ao colocar . rating no final ele so mostra a nota se eu tiro ele coloca todas as infos do filme 1
+meanR1 = (ratings.query("movieId==1").rating.mean())
+print("Mean rating movie 1 = ", meanR1)
+#PAREI NO MIN 4:45
